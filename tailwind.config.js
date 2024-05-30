55deff1549ab8ee08bd4d0727b3ebb76b18e8b7d
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-05-30 21:09:44 +0900

    トップページにアクセスできるように

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import daisyui from "daisyui";
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+      daisyui,
+  ],
+  daisyui: {
+    themes: ['retro'],
+  },
 }
