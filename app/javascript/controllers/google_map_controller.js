0003cae0feca23ec10ddc9fdfa3473a9da62b3eb
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-05-31 04:03:07 +0900

    レストランの詳細情報を確認できるように

--- app/javascript/controllers/google_map_controller.js
+++ app/javascript/controllers/google_map_controller.js
@@ -4,6 +4,7 @@ import { Loader } from "@googlemaps/js-api-loader"
 // Connects to data-controller="google-map"
 export default class extends Controller {
   static values = {apiKey: String, mapId: String};
+  static targets = ['drawer', 'placeName', 'placePhoto', 'placeRating', 'placeSummary', 'placeReviews', 'map']
 
   initialize() {
     this.loader = new Loader({ apiKey: this.apiKeyValue, version: "weekly" });
@@ -27,7 +28,7 @@ export default class extends Controller {
     this.loader.load().then(async () => {
       const { Map } = await google.maps.importLibrary("maps");
       const { PinElement, AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-      this.map = new Map(this.element, {
+      this.map = new Map(this.mapTarget, {
         center: this.position,
         zoom: 15,
         mapId: this.mapIdValue,
@@ -49,10 +50,32 @@ export default class extends Controller {
       const { places } = await this.searchNearby();
       this.displyPlaces = await places;
       for (const place of this.displyPlaces) {
-        new AdvancedMarkerElement({
+        const advancedMarkerElement = new AdvancedMarkerElement({
           map: this.map,
           position: { lat: place.location.latitude, lng: place.location.longitude },
           title: place.displayName.text,
+        });
+        advancedMarkerElement.addListener('click', async () => {
+          this.drawerTarget.classList.remove('hidden');
+          const photo = place.photos[0]
+          this.placePhotoTarget.src = this.getPlacePhotoUrl(photo.name);
+          this.placeNameTarget.innerText = place.displayName.text;
+          this.placeRatingTarget.innerText = `評価: ${place.rating}`;
+          if (place.editorialSummary?.text) {
+            this.placeSummaryTarget.innerText = place.editorialSummary.text;
+          } else {
+            this.placeSummaryTarget.innerText = '';
+          }
+          this.placeReviewsTarget.innerText = '';
+          for (const review of place.reviews) {
+            const item = document.createElement("div");
+            if (review?.text?.text) {
+              item.innerText = review?.text?.text;
+              item.classList.add('carousel-item');
+              item.classList.add('w-full');
+              this.placeReviewsTarget.appendChild(item);
+            }
+          }
         })
       }
     });
@@ -63,11 +86,12 @@ export default class extends Controller {
     const headers = {
       'Content-Type': 'application/json',
       'X-Goog-Api-Key': this.apiKeyValue,
-      'X-Goog-FieldMask': 'places.displayName,places.location',
+      'X-Goog-FieldMask': 'places.displayName,places.location,places.photos,places.editorialSummary,places.rating,places.reviews',
     }
     const method = 'POST';
     const data =
       {
+        "includedTypes": ['restaurant'],
         "locationRestriction": {
           "circle": {
             "center": {
@@ -81,5 +105,14 @@ export default class extends Controller {
     const body = JSON.stringify(data);
     const options = { headers, method, body };
     return fetch(url, options).then(response => response.json());
+  }
+
+  getPlacePhotoUrl(photoName) {
+    const url = new URL(`https://places.googleapis.com/v1/${photoName}/media`);
+    const maxWidthPx = 400;
+    const maxHeightPx = 400;
+    const key = this.apiKeyValue;
+    url.search = new URLSearchParams({ maxWidthPx, maxHeightPx, key });
+    return url;
   }
 }
