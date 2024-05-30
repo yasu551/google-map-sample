2a3f161a349084d587124bf1eeacfaa68cd09bc5
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-05-30 22:56:52 +0900

    Google Map上に現在地のピンが立つように

--- app/javascript/controllers/google_map_controller.js
+++ app/javascript/controllers/google_map_controller.js
@@ -3,19 +3,43 @@ import { Loader } from "@googlemaps/js-api-loader"
 
 // Connects to data-controller="google-map"
 export default class extends Controller {
-  static values = {apiKey: String};
+  static values = {apiKey: String, mapId: String};
 
   initialize() {
     this.loader = new Loader({ apiKey: this.apiKeyValue, version: "weekly" });
+    navigator.geolocation.getCurrentPosition((geolocationPosition) => {
+      this.position =  { lat: geolocationPosition.coords.latitude, lng: geolocationPosition.coords.longitude };
+    }, () => {
+      this.position =  { lat: -34.397, lng: 150.644 };
+    })
   }
 
   connect() {
+    const intervalId = setInterval(() => {
+      if (this.position) {
+       clearInterval(intervalId);
+      }
+    }, 1000);
+
     this.loader.load().then(async () => {
       const { Map } = await google.maps.importLibrary("maps");
-      map = new Map(this.element, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+      const { PinElement, AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+      this.map = new Map(this.element, {
+        center: this.position,
+        zoom: 14,
+        mapId: this.mapIdValue,
       });
+      const currentPositionPin = new PinElement({
+        background: '#00008B',
+        glyphColor: '#87CEEB',
+        borderColor: '#87CEEB',
+      });
+      const marker = new AdvancedMarkerElement({
+        map: this.map,
+        position: this.position,
+        title: 'Uul',
+        content: currentPositionPin.element,
+      })
     });
   }
 }
